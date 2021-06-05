f67b45e301f4a22d055c6de4d6a4fc4f1e864411
Anil Rawat <rawatanil488@gmail.com>
2021-06-05 15:36:15 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import {Button} from 'react-bootstrap';
-function App() {
-  return (
-    <div className="App">
-      <Button bsStyle="primary">Hello I am a button</Button>
-    </div>
-  );
+import {Component} from 'react';
+import {Button, Card} from 'react-bootstrap';
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      tasksList: [],
+      task: {
+        id: 0,
+        priority: "",
+        taskName: "",
+        taskDetails: "",
+        status: ""
+      }
+    };
+  }
+  addTask () {
+    console.log("I will add task for you")
+  }
+  render () {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Task Manager</h1>
+        </div>
+        <div className="App-body">
+          <div className="d-flex justify-content-end align-items-center p-2">
+            <Button className="btn btn-primary" onClick={this.addTask}>
+              <FontAwesomeIcon icon={faPlus} /> Add Task
+            </Button>
+          </div>
+          <div>
+            <Card></Card>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
