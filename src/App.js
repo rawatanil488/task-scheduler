e03f8af6e9b965b91958dd34763525c665e1b5f1
Anil Rawat <rawatanil488@gmail.com>
2021-06-05 13:15:40 +0530

    added node sass and scsss files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Button} from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-		Hello
+      <Button bsStyle="primary">Hello I am a button</Button>
     </div>
   );
 }
